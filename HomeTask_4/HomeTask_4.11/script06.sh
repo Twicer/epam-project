23eeb74f3adfdd16444e4042330dbf5e1dd51732
Oleksandr Semencha <asemencha@outlook.com>
2020-05-07 15:07:51 +0300

    master

---
+++ HomeTask_4/HomeTask_4.11/script06.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# task 4.11.6
+# Develop a script that invokes the grep command and takes the following arguments:
+# - The text to find
+# - file in which to find this text;
+# - The maximum number of lines to display.
---
+++ HomeTask_4/HomeTask_4.11/script07.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# task 4.11.7
+# Run 4.11.6 in restricted mode for the /home/user directory
